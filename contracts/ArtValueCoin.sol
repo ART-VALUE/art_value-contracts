361a176d75873437aeac0dceca259bbf854a3a9a
Pieter Fiers <pieter@pfiers.net>
2021-03-18 20:26:53 +0100

    ArtValueCoin: add mint() & burn()

--- contracts/ArtValueCoin.sol
+++ contracts/ArtValueCoin.sol
@@ -6,13 +6,39 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC777/ERC777Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract ArtValueCoin is Initializable, ERC777Upgradeable {
+contract ArtValueCoin is Initializable, ERC777Upgradeable, AccessControlUpgradeable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+
     function initialize(
         string memory name,
         string memory symbol,
         address[] memory defaultOperators
     ) public virtual initializer {
+        __AccessControl_init();
         __ERC777_init(name, symbol, defaultOperators);
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+
+    function mint(
+        address to,
+        uint256 amount,
+        bytes memory userData,
+        bytes memory operatorData
+    ) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        _mint(to, amount, userData, operatorData);
+    }
+
+    function burn(
+        address from,
+        uint256 amount,
+        bytes memory userData,
+        bytes memory operatorData
+    ) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a burner");
+        _burn(from, amount, userData, operatorData);
     }
 }
