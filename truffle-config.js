7c43729c6bd4e2cd791c61aea4bba936a4c890c7
Pieter Fiers <pieter@pfiers.net>
2021-03-18 20:29:10 +0100

    Add ts support

--- truffle-config.js
+++ truffle-config.js
@@ -26,7 +26,7 @@
 const path = require('path');
 
 module.exports = {
-  contracts_build_directory: path.join(__dirname, "/build"),
+  // contracts_build_directory: path.join(__dirname, "/build"),
 
   /**
    * Networks define how you connect to your ethereum client and let you set the
